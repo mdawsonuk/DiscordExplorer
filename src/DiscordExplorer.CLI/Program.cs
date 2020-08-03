01ad86e8ed9b01583d25309f35e8ebc6447df719
Sam Leonard <samuelleonard35@googlemail.com>
2020-08-03 01:33:42 +0100

    Added basic CLI interface.

--- src/DiscordExplorer.CLI/Program.cs
+++ src/DiscordExplorer.CLI/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using System.IO;
+using CommandLine;
 using DiscordExplorer.CacheParser;
 
 namespace DiscordExplorer.CLI
 {
+    public class Options
+    {
+        [Option('i', "index",
+                Required = false,
+                HelpText = "Specify the index file to use"
+        )]
+        public string Index { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            IndexParse.parse();
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o => 
+                {
+                    if (o.Index != "") {
+                        Console.WriteLine($"Current Arguments: -i {o.Index}");
+                        IndexParse.parse(o.Index);
+                    }
+                }
+            );
         }
     }
 }
--- src/DiscordExplorer.CacheParser/IndexParse.cs
+++ src/DiscordExplorer.CacheParser/IndexParse.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.IO;
 
 namespace DiscordExplorer.CacheParser
 {
     public static class IndexParse
     {
-        public static void parse() {
-            Console.WriteLine("Hello World! from CacheParser.IndexParse");
+        public static void parse(string indexFile) {
+            if (string.IsNullOrEmpty(indexFile)) {
+                indexFile = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    "discord\\Cache\\index"
+                );
+            }
+
+            Console.WriteLine($"Parsing index file: '{indexFile}'");
         }
     }
 }
