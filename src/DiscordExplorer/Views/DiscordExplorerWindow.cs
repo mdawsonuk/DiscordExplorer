e62c769eeb4c1bd5e17f9d01fdbb35fdad416205
Matt <matthewd102@hotmail.com>
2020-08-24 14:07:18 +0100

    State and location for UI window is now saved

--- src/DiscordExplorer/Views/DiscordExplorerWindow.cs
+++ src/DiscordExplorer/Views/DiscordExplorerWindow.cs
@@ -46,6 +46,43 @@ namespace DiscordExplorer
             ProfilesSplitContainer.SplitterDistance = 1600;
             ProfilesSplitContainer.SplitterWidth = 20;
 
+            // Taken from https://stackoverflow.com/questions/937298/restoring-window-size-position-with-multiple-monitors
+            WindowState = FormWindowState.Maximized;
+            StartPosition = FormStartPosition.WindowsDefaultBounds;
+
+            // check if the saved bounds are nonzero and visible on any screen
+            if (Settings.Default.WindowPosition != Rectangle.Empty &&
+                IsVisibleOnAnyScreen(Settings.Default.WindowPosition))
+            {
+                // first set the bounds
+                StartPosition = FormStartPosition.Manual;
+                DesktopBounds = Settings.Default.WindowPosition;
+
+                // afterwards set the window state to the saved value (which could be Maximized)
+                WindowState = Settings.Default.WindowState;
+            }
+            else
+            {
+                // this resets the upper left corner of the window to windows standards
+                StartPosition = FormStartPosition.WindowsDefaultLocation;
+
+                // we can still apply the saved size
+                Size = Settings.Default.WindowPosition.Size;
+            }
+        }
+
+        private bool IsVisibleOnAnyScreen(Rectangle rect)
+        {
+            // Taken from https://stackoverflow.com/questions/937298/restoring-window-size-position-with-multiple-monitors
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen.WorkingArea.IntersectsWith(rect))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         #region Extra components
@@ -248,6 +285,33 @@ namespace DiscordExplorer
                 new DiscordProfile(dummy, "Test User 2", "0000", "TestAvatar", DateTime.UtcNow.AddDays(-100)),
             };
             ProfilesData.AutoResizeColumns();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            // Taken from https://stackoverflow.com/questions/937298/restoring-window-size-position-with-multiple-monitors
+
+            base.OnClosed(e);
+
+            // only save the WindowState if Normal or Maximized
+            switch (WindowState)
+            {
+                case FormWindowState.Normal:
+                case FormWindowState.Maximized:
+                    Settings.Default.WindowState = WindowState;
+                    break;
+                default:
+                    Settings.Default.WindowState = FormWindowState.Normal;
+                    break;
+            }
+
+            // reset window state to normal to get the correct bounds
+            // also make the form invisible to prevent distracting the user
+            Visible = false;
+            WindowState = FormWindowState.Normal;
+
+            Settings.Default.WindowPosition = DesktopBounds;
+            Settings.Default.Save();
         }
     }
 }
