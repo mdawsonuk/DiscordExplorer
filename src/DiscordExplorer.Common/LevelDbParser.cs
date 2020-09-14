ea1f6d491dd833174d520a86e296361978f69644
Matt <matthewd102@hotmail.com>
2020-09-14 23:43:46 +0100

    Added LevelDBParser.go as standalone exe to list DB key-value pairs

--- src/DiscordExplorer.Common/LevelDbParser.cs
+++ src/DiscordExplorer.Common/LevelDbParser.cs
@@ -1,13 +1,61 @@
-﻿using LevelDB;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace DiscordExplorer.Common
 {
     public class LevelDbParser
     {
+        /// <summary>
+        /// User ID of the Local User
+        /// </summary>
         public long LocalUserID { get; private set; }
 
+        /// <summary>
+        /// Email Address of the local user
+        /// </summary>
         public string LocalUserEmail { get; private set; }
+
+        /// <summary>
+        /// Searches made by the local user in servers/guilds
+        /// </summary>
+        public string UserSearches { get; private set; }
+
+        /// <summary>
+        /// Draft messages by the local user
+        /// </summary>
+        public string UserDrafts { get; private set; }
+
+        /// <summary>
+        /// The recently used emojis by the local user
+        /// </summary>
+        public string UserEmojis { get; private set; }
+
+        /// <summary>
+        /// Recently played games
+        /// </summary>
+        public string GameStore { get; private set; }
+
+        /// <summary>
+        /// Token for the user
+        /// </summary>
+        public string UserToken { get; private set; }
+
+        public string GifFavourites { get; private set; }
+
+        public string SelectedChannelStore { get; private set; }
+
+        #region Timestamp Evidence
+
+        public long LastVoiceFeedback { get; private set; }
+
+        public long LastNonRequiredUpdateShown { get; private set; }
+
+        public long LastHiddenChannelNotice { get; private set; }
+
+        public DateTime LastChangeLogDate { get; private set; }
+
+        #endregion
 
         /// <summary>
         /// Creates a parser for the given LevelDB directory
@@ -25,14 +73,76 @@ namespace DiscordExplorer.Common
                 throw new DirectoryNotFoundException($"Directory {directory} not found");
             }
 
-            var options = new Options
+            var proc = new Process
             {
-                CreateIfMissing = false,
-                ParanoidChecks = true,
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c DiscordExplorer.LevelDBParser.exe \"" + directory + "\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                }
             };
-            using (var db = new DB(options, directory))
+            Console.WriteLine(Environment.CurrentDirectory);
+            proc.Start();
+            string output = proc.StandardOutput.ReadToEnd();
+            string[] lines = output.Split("\n");
+            for (int i = 0; i < lines.Length; i++)
             {
-                
+                if (lines[i].EndsWith("user_id_cache"))
+                {
+                    LocalUserID = long.Parse(lines[i + 1][1..^1]);
+                }
+                if (lines[i].EndsWith("email_cache"))
+                {
+                    LocalUserEmail = lines[i + 1][1..^1];
+                }
+                if (lines[i].EndsWith("DraftStore"))
+                {
+                    UserDrafts = lines[i + 1];
+                }
+                if (lines[i].EndsWith("EmojiStore"))
+                {
+                    UserEmojis = lines[i + 1];
+                }
+                if (lines[i].EndsWith("SearchStore"))
+                {
+                    UserSearches = lines[i + 1];
+                }
+                if (lines[i].EndsWith("SearchStore"))
+                {
+                    GameStore = lines[i + 1];
+                }
+                if (lines[i].EndsWith("token"))
+                {
+                    UserToken = lines[i + 1][1..^1];
+                }
+                if (lines[i].EndsWith("GIFFavoritesStore"))
+                {
+                    GifFavourites = lines[i + 1];
+                }
+                if (lines[i].EndsWith("SelectedChannelStore"))
+                {
+                    SelectedChannelStore = lines[i + 1];
+                }
+                if (lines[i].EndsWith("lastVoiceFeedback"))
+                {
+                    LastVoiceFeedback = long.Parse(lines[i + 1]);
+                }
+                if (lines[i].EndsWith("lastNonRequiredUpdateShown"))
+                {
+                    LastNonRequiredUpdateShown = long.Parse(lines[i + 1]);
+                }
+                if (lines[i].EndsWith("lastHiddenChannelNotice"))
+                {
+                    LastHiddenChannelNotice = long.Parse(lines[i + 1]);
+                }
+                if (lines[i].EndsWith("lastChangeLogDate"))
+                {
+                    LastChangeLogDate = DateTime.Parse(lines[i + 1][1..^1]);
+                }
             }
         }
     }
